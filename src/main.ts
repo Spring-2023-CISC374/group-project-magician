a6be55e04781c35a82f4012a55b0c37ff0bd4b29
Sydney <sydneyph@udel.edu>
2023-03-19 00:11:13 -0400

    added scenes to main and edited index.html to update the names the files were called

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import Phaser from 'phaser'
+import combat_1 from './scenes/combat_1'
+
+import home from './scenes/home'
+import inventory from './scenes/inventroy'
+import level_1 from './scenes/level_1'
+import map from './scenes/map'
+import start from './scenes/start'
 
 
 const config: Phaser.Types.Core.GameConfig = {
@@ -13,7 +20,7 @@ const config: Phaser.Types.Core.GameConfig = {
 			debug: false
 		},
 	},
-	scene: [],
+	scene: [start, home, map, level_1, combat_1, inventory],
 }
 
 export default new Phaser.Game(config)
