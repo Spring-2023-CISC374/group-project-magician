0a59b2c4252db7edc84503659067d45055c19322
Sean Johnson <seanjohn@udel.edu>
2023-03-20 13:35:08 -0400

    Added enemy and sprite in combat scene

--- src/scenes/combat_1.ts
+++ src/scenes/combat_1.ts
@@ -1,16 +1,19 @@
 import Phaser from 'phaser'
-
 export default class combat_1 extends Phaser.Scene {
+	private player?: Phaser.Physics.Arcade.Sprite
 	constructor() {
 		super('combat_1')
 	}
 
 	preload() {
-		//load image  for start screen here
+		this.load.spritesheet('enemyDragon', 'assets/enemyDragon.png',
+		{frameWidth: 100, frameHeight:100})
 	}
 
 	create() {		
-
+		this.makeEnemy();
+		this.makeCharacter();
+		this.setCharPosition();
         this.add.text(0, 40, 'Currently in Combat \n Click for inventory', {
 			fontSize: '32px',
 			color: '#ffffff'
@@ -27,4 +30,16 @@ export default class combat_1 extends Phaser.Scene {
 		//
 	}
 
+	private makeEnemy() {
+		this.player = this.physics.add.sprite(400,300,'enemyDragon')
+		this.player.setCollideWorldBounds(true)
+	}
+	private setCharPosition() {
+		this.player?.setPosition(200,300)
+	}
+	private makeCharacter() {
+		this.player = this.physics.add.sprite(80,510,'mainChar')
+		this.player.setScale(2);
+		this.player.setCollideWorldBounds(true)
+	}
 }
