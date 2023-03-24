4076d256f80b52f8d6e4297696628029b5538df6
Ren Ross <briross@udel.edu>
2023-03-24 03:54:23 -0400

    anims added to player sprite idle animation casting animation

--- src/scripts/scenes/combat_1.ts
+++ src/scripts/scenes/combat_1.ts
@@ -8,6 +8,8 @@ export default class combat_1 extends Phaser.Scene {
 	constructor() { super('combat_1') }
 
 	preload() {
+		this.load.spritesheet('player', 'assets/player.png',
+		{frameWidth: 32, frameHeight: 32})
 		this.load.spritesheet('enemyDragon', 'assets/enemyDragon.png',
 		{frameWidth: 100, frameHeight: 100})
 		this.load.spritesheet('darkSpell', 'assets/darkSpell.png',
@@ -28,8 +30,9 @@ export default class combat_1 extends Phaser.Scene {
             this.scene.stop('combat_1')
             this.scene.start('inventory')
 		})
+		this.player.anims.play('idle', true)
 		// collision between spell and enemy
-		this.physics.add.overlap(this.enemy, this.spell, this.handleSpellCollision, undefined, this)
+		this.physics.add.overlap(this.enemy, this.spell, this.handleSpell, undefined, this)
 	}
 	
 
@@ -51,10 +54,24 @@ export default class combat_1 extends Phaser.Scene {
 		this.player?.setPosition(200,300)
 	}
 	private makeCharacter() {
-		this.player = this.physics.add.sprite(80, 510, 'mainChar')
+		this.player = this.physics.add.sprite(80, 510, 'player')
 		this.player
 			.setScale(2)
 			.setCollideWorldBounds(true)
+		this.anims.create({
+				key: 'idle', 
+				frames: this.anims.generateFrameNumbers('player', {
+					start: 0, end: 1
+				}), 
+				frameRate: 5, repeat: -1
+			})
+		this.anims.create({
+			key: 'cast', 
+			frames: this.anims.generateFrameNumbers('player', {
+				start: 65, end: 68
+			}), 
+			frameRate: 8
+		})
 		return this.player
 	}
 	private makeSpell(player: Phaser.GameObjects.Sprite) {
@@ -63,25 +80,27 @@ export default class combat_1 extends Phaser.Scene {
 			.setVisible(false)
 			.setCollideWorldBounds(true)
 		this.anims.create({
-			key: 'dark_spell', 
-			frames: this.anims.generateFrameNumbers('darkSpell', {
-				start: 0, end: 6
-			}), 
-			frameRate: 10, repeat: -1
-		}) 		
+				key: 'dark_spell', 
+				frames: this.anims.generateFrameNumbers('darkSpell', {
+					start: 0, end: 6
+				}), 
+				frameRate: 10, repeat: -1
+			}) 		
 		return this.spell
 	}
 	private castSpell() {
-		this.spell?.setVisible(true)
-		this.spell?.anims.play('dark_spell', true)
-		this.spell?.setX(this.spell.x + 1)
+		this.player?.anims
+			.play('cast', true)
+			.once('animationcomplete', () => {
+				this.spell?.setVisible(true)
+				this.spell?.anims.play('dark_spell', true)
+				this.spell?.setX(this.spell.x + 1)
+				this.player?.anims.play('idle', true)
+			});	
 	}
-	private handleSpellCollision(enemy: Phaser.GameObjects.GameObject, spell: Phaser.GameObjects.GameObject) {
+	// collision between spell and enemy handler
+	private handleSpell(enemy: Phaser.GameObjects.GameObject, spell: Phaser.GameObjects.GameObject) {
 		(spell as Phaser.Physics.Arcade.Image).disableBody(true, true);
 		(enemy as Phaser.Physics.Arcade.Image).setTint(0xff0000);
-
-
-
-
 	}
 }
