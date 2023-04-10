34b95a10ceb30a6236122770dbc5be6de60805a9
Sydney <sydneyph@udel.edu>
2023-04-10 15:16:20 -0400

    commiting b4 merge

--- src/scripts/scenes/home.ts
+++ src/scripts/scenes/home.ts
@@ -9,7 +9,7 @@ export default class home extends Phaser.Scene {
 	}
 
 	create() {	
-		const background = this.physics.add.image(500, 300, 'home_Background').setScale(1.5)
+		this.physics.add.image(450, 300, 'home_Background').setScale(1.455)
 		const home_marker = this.physics.add.image(500,400,'home_marker')
 
 		this.createEmitter("petal"); // cpde for this taken from blog.ourcase.co
