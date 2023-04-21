64915467060cb6ab61e6d62dd95667c31b16ab1d
Ren Ross <briross@udel.edu>
2023-04-21 00:15:43 -0400

    fixed compile errors in combat_1

--- src/scripts/scenes/combat_1.ts
+++ src/scripts/scenes/combat_1.ts
@@ -7,15 +7,15 @@ export default class combat_1 extends Phaser.Scene {
 	private enemy?: Phaser.Physics.Arcade.Sprite
 	private spell?: Phaser.Physics.Arcade.Sprite
 	private keys?: Phaser.Types.Input.Keyboard.CursorKeys;
-	private currentHealth: number
+	private currentHealth?: number
 	private playerTurn: boolean
 	private enemyHealth: number
 	private spellDamage: number
 	private isDisabled: boolean
-	private enemyText: Phaser.GameObjects.Text
-	private enemyAttack: Phaser.GameObjects.Text
-	private playerAttack: Phaser.GameObjects.Text
-	private timer: Phaser.Time.Clock
+	private enemyText?: Phaser.GameObjects.Text
+	private enemyAttack?: Phaser.GameObjects.Text
+	private playerAttack?: Phaser.GameObjects.Text
+	private timer?: Phaser.Time.Clock
 
 	constructor() { 
 		super('combat_1') 
@@ -50,7 +50,7 @@ export default class combat_1 extends Phaser.Scene {
 		bg.setScale(
 			this.cameras.main.width/bg.width, this.cameras.main.height/bg.height)		
 		// create assets
-		this.player = new MainCharacter(this, 80, 515, this.currentHealth)
+		this.player = new MainCharacter(this, 80, 515, this.currentHealth as number)
 		this.player.displayHealth()
 		this.enemy = this.makeEnemy()
 		this.spell = this.makeSpell(this.player)
@@ -66,9 +66,8 @@ export default class combat_1 extends Phaser.Scene {
 		this.enemy.anims.play('enemyIdle', true)
 		// add collisions
 		this.physics.add.overlap(this.enemy, this.spell, this.handleSpell, undefined, this)
-		
-
-		this.add.existing(new Click_Change_Scene(this, 770, 525, 'button', () => {			// create button to go to map
+		// create button to go to map
+		this.add.existing(new Click_Change_Scene(this, 770, 525, 'button', () => {			
 			this.scene.start('inventory')											
 			this.scene.stop('combat_1')
 		}));
@@ -96,22 +95,23 @@ export default class combat_1 extends Phaser.Scene {
     }
 
 	update() {
-
-		this.enemyText.setText('Enemy health is: ' + this.enemyHealth)
+		this.enemyText?.setText('Enemy health is: ' + this.enemyHealth)
 		this.player?.setText()
-		// update spells
+		this.currentHealth = this.currentHealth as number;
+		this.enemyAttack = this.enemyAttack as Phaser.GameObjects.Text;
 		if (this.enemyHealth === 0) {
-			this.handleDeath(this.enemy)
+			this.handleDeath(this.enemy as Phaser.Physics.Arcade.Sprite)
 		}
+		// update spells
 		if (this.keys?.space.isDown) { 
-			this.player?.castSpell(this.player,this.spell)
+			this.player?.castSpell(this.player, this.spell as Phaser.Physics.Arcade.Sprite)
 		}
 		if (this.spell?.active == true) {
 			this.spell.setX(this.spell.x + 2)
 		}
 		if (this.isDisabled == true) {
-			this.handleEnemyAttack()
-			this.resetSpellPosition()
+			this.handleEnemyAttack(this.enemyAttack, this.currentHealth)
+			this.resetSpellPosition(this.player as Phaser.GameObjects.Sprite)
 		}
 		
 	}
@@ -149,44 +149,41 @@ export default class combat_1 extends Phaser.Scene {
 		return this.spell
 	}
 
-	private handleDeath(enemy: Phaser.GameObjects.GameObject,) {
+	private handleDeath(enemy: Phaser.GameObjects.GameObject) {
 		(enemy as Phaser.Physics.Arcade.Image).setTint(0xff0000);
 		this.enemy?.anims.stop();
-
 		this.exit_combat();
 	}
-	private handleSpell(enemy:Phaser.GameObjects.GameObject, spell: Phaser.GameObjects.GameObject) {
-		(spell as Phaser.Physics.Arcade.Image).disableBody(true, true);
-		this.handleCharacterAttack()
-		this.isDisabled = true
 
+	private handleSpell(enemy: Phaser.GameObjects.GameObject, spell: Phaser.GameObjects.GameObject) {
+		(spell as Phaser.Physics.Arcade.Image).disableBody(true, true);
+		this.handleCharacterAttack(this.playerAttack as Phaser.GameObjects.Text);
+		this.isDisabled = true
 	}
-	private handleEnemyAttack() {
-		this.currentHealth -= 10;
-		this.player?.setHealth(this.player?.getHealth()-10)
-		this.enemyAttack.setVisible(true)
+
+	private handleEnemyAttack(enemyAttack: Phaser.GameObjects.Text, currentHealth: number) {
+		currentHealth -= 10;
+		console.log(currentHealth)
+		this.player?.setHealth(this.player?.getHealth()-10);
+		enemyAttack.setVisible(true);
 		setTimeout(()=> {
-			this.enemyAttack.setVisible(false)
-		}, 4000)	
-		
-}
+			enemyAttack.setVisible(false)
+		}, 4000);	
+	}
 	
-	private handleCharacterAttack() {
+	private handleCharacterAttack(playerAttack: Phaser.GameObjects.Text) {
 		this.enemyHealth -= this.spellDamage;
-		this.playerAttack.setVisible(true)
+		playerAttack?.setVisible(true)
 		setTimeout(()=> {
-			this.playerAttack.setVisible(false)
-		}, 4000)	
+			playerAttack?.setVisible(false)
+		}, 4000)
 	}
-	private resetSpellPosition() {
-		this.spell?.enableBody(true, this.player.x + 30, this.player.y, true, false)
+
+	private resetSpellPosition(player: Phaser.GameObjects.Sprite) {
+		this.spell?.enableBody(true, player.x + 30, player.y, true, false)
 		this.spell?.setActive(false)
 		this.isDisabled = false;
-
-
 	}
-
-
 
 	private exit_combat(){
 		this.add.text(400, 45, 'Combat Finished', {
