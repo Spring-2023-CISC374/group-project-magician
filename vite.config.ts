14e9d3d62f4de09cfce63c02058841e9d3c27945
Sydney <sydneyph@udel.edu>
2023-04-26 13:55:46 -0400

    no local errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [],
-  server: { host: "0.0.0.0", port: 8000 },
-  clearScreen: false,
-  root: ".",
-  base: "./",
-});
+	plugins: [],
+	server: { host: '0.0.0.0', port: 8000 },
+	clearScreen: false,
+	base: "/group-project-magician/"
+})
